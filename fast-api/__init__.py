f5575126e0e6f4963fcfad857209b5ad48f0c5b6
Yogesh Sharma <yosharma@linkedin.com>
2022-09-20 16:49:42 +0530

    sample code

--- fast-api/__init__.py
+++ fast-api/__init__.py
@@ -1,11 +1,32 @@
-import logging
-
 import azure.functions as func
-from fastapp.api import app
+import time
 import nest_asyncio
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 
 nest_asyncio.apply()
 
+app = FastAPI(title="FastAPI on Azure")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+@app.get("/")
+async def root():
+    return {"message": "Hello from Azure Function"}
+    
 
 async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     return func.AsgiMiddleware(app).handle(req, context)
